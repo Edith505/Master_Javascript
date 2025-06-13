e9a7f62bd47d8034892f029c02c5a2592ba0a39c
EDITH-RAND\edith <edithrand505@gmail.com>
2025-06-13 13:20:28 -0500

    Exercice 1

---
+++ Exercices_1/script.js
@@ -0,0 +1,6 @@
+let prenom = prompt("Quel est votre prénom ?");
+let nom = prompt("Quel est votre nom ?");
+let age = parseInt(prompt("Quel est votre âge ?"));
+const ENFANCE = 5;
+
+alert(`Sapristi! On ne m'avait pas prévenu que c'était vous ${prenom} ! Euh... Je veux dire... Monsieur le grand magicien ${nom} ! Cela fait déjà au moins ${age - ENFANCE} ans que vous faites rayonner notre contrée!`);
