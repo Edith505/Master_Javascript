4a4b82dca579b3b49836c6483433dfba835331f2
EDITH-RAND\edith <edithrand505@gmail.com>
2025-06-19 22:47:22 -0500

    Exercice 2.2a

---
+++ Exercice_2/js/script2.2a.js
@@ -0,0 +1,10 @@
+"use strict"
+let nom_1 = prompt("Entrez votre nom :")
+let nom_2 = prompt("Entrez le nom de votre ami :")
+
+
+let note_1 = document.getElementById("note_1")
+let moyenne_1 = document.getElementById("moyenne_1")
+let note_2 = document.getElementById("note_2")
+let moyenne_2 = document.getElementById("moyenne_2")
+let meilleur_moyenne = document.getElementById("meilleur")
